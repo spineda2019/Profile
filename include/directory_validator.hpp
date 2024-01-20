b6907240c44dded396a75972db67ccc9ee9b6aac
spineda2019 <spineda@wpi.edu>
2024-01-20 15:26:05 -0500

    implementing getting files from directory

--- include/directory_validator.hpp
+++ include/directory_validator.hpp
@@ -1,6 +1,14 @@
 #ifndef PROFILE_INCLUDE_DIRECTORY_VALIDATOR_HPP
 #define PROFILE_INCLUDE_DIRECTORY_VALIDATOR_HPP
+
+#include <array>
+#include <filesystem>
+#include <vector>
+
 namespace directory_validation {
-bool DirectoryExists(const char* directory_name);
-}
+bool DirectoryExists(std::filesystem::path directory_name);
+
+void GetFilesToProfile(const std::filesystem::path& directory,
+                       std::vector<std::filesystem::path>& found_files);
+}  // namespace directory_validation
 #endif  // PROFILE_INCLUDE_DIRECTORY_VALIDATOR_HPP
--- src/directory_validator.cpp
+++ src/directory_validator.cpp
@@ -1,43 +1,24 @@
 #include <directory_validator.hpp>
-#ifdef _WIN32
-#include <windows.h>
-#elif __APPLE__
-#include <sys/stat.h>
-#elif __linux__
-#include <sys/stat.h>
-#endif
+#include <filesystem>
+#include <vector>
 
 namespace directory_validation {
-bool DirectoryExists(const char* directory_name) {
-#ifdef _WIN32
-  DWORD ftyp = GetFileAttributesA(directory_name);
-  if (ftyp == INVALID_FILE_ATTRIBUTES) {
-    return false;  // something is wrong with your path!
-  }
-  if (ftyp & FILE_ATTRIBUTE_DIRECTORY) {
-    return true;  // this is a directory!
-  }
-  return false;  // this is not a directory!
-#elif __APPLE__
-  struct stat info;
-
-  if (stat(directory_name, &info) != 0)
+bool DirectoryExists(std::filesystem::path directory_name) {
+  if (!std::filesystem::exists(directory_name)) {
     return false;
-  else if (info.st_mode & S_IFDIR)
+  } else {
     return true;
-  else
-    return false;
-#elif __linux__
-  struct stat info;
+  }
+}
 
-  if (stat(directory_name, &info) != 0)
-    return false;
-  else if (info.st_mode & S_IFDIR)
-    return true;
-  else
-    return false;
-#else
-  return false;
-#endif
+void GetFilesToProfile(const std::filesystem::path& directory,
+                       std::vector<std::filesystem::path>& found_files) {
+  if (std::filesystem::is_directory(directory)) {
+    for (const auto& entry : std::filesystem::directory_iterator(directory)) {
+      GetFilesToProfile(entry.path(), found_files);
+    }
+  } else if (std::filesystem::is_regular_file(directory)) {
+    found_files.push_back(directory);
+  }
 }
 }  // namespace directory_validation
