d5052d019f894020f75d04b48bc3e77d7a019d67
spineda2019 <spineda@wpi.edu>
2024-01-20 14:42:16 -0500

    return early if directory exists

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
-int main() {}
+#include <iostream>
+
+#include "directory_validator.hpp"
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "USE: Profile <DIRECTORY>" << std::endl;
+  }
+  const char* directory = argv[1];
+
+  if (!directory_validation::DirectoryExists(directory)) {
+    std::cerr << "FATAL: Directory " << directory << "does not exist!"
+              << std::endl;
+    return -1;
+  }
+}
