46e92fcab8a050f0a306827ce9aea412956a7e4b
spineda2019 <spineda@wpi.edu>
2024-01-20 14:41:42 -0500

    added stub to verify directory existance

---
+++ include/directory_validator.hpp
@@ -0,0 +1,6 @@
+#ifndef PROFILE_INCLUDE_DIRECTORY_VALIDATOR_HPP
+#define PROFILE_INCLUDE_DIRECTORY_VALIDATOR_HPP
+namespace directory_validation {
+bool DirectoryExists(const char* directory_name);
+}
+#endif  // PROFILE_INCLUDE_DIRECTORY_VALIDATOR_HPP
---
+++ src/directory_validator.cpp
@@ -0,0 +1,5 @@
+#include <directory_validator.hpp>
+
+namespace directory_validation {
+bool DirectoryExists(const char* directory_name) { return false; }
+}  // namespace directory_validation
