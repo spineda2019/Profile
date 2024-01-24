482e401a887ad388641d57a3bd1e3ca6f39226e2
spineda2019 <spineda@wpi.edu>
2024-01-24 13:23:24 -0500

    add known file extensions for extension checking

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,6 +1,7 @@
 #ifndef PROFILE_INCLUDE_PARSER_HPP
 #define PROFILE_INCLUDE_PARSER_HPP
 
+#include <array>
 #include <filesystem>
 #include <vector>
 
@@ -11,6 +12,10 @@ class Parser {
   void ParseFiles() const;
 
  private:
+  const bool IsValidFile(const std::filesystem::path& file) const;
   const std::vector<std::filesystem::path> files_;
+  static constexpr std::array<const char*, 8> double_slash_extensions_{
+      ".c", ".cpp", ".h", ".hpp", ".js", ".rs", ".ts", ".zig"};
+  static constexpr std::array<const char*, 1> pound_sign_extensions_{".py"};
 };
 #endif  // PROFILE_INCLUDE_PARSER_HPP
