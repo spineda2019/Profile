79d75e87a31bb63b3ee5bde217b2a6ec88a9629a
spineda2019 <spineda.wpi.alum@gmail.com>
2024-04-10 16:22:38 -0400

    move include directory to src

--- src/include/directory_validator.hpp
+++ src/include/directory_validator.hpp
@@ -20,40 +20,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#ifndef INCLUDE_DIRECTORY_VALIDATOR_HPP_
+#define INCLUDE_DIRECTORY_VALIDATOR_HPP_
+
 #include <filesystem>
-#include <iostream>
+#include <vector>
 
-#include "include/Parser.hpp"
-#include "include/directory_validator.hpp"
+namespace directory_validation {
+bool DirectoryExists(const std::filesystem::path& directory_name);
 
-int main(int argc, char** argv) {
-  std::filesystem::path directory{};
-
-  if (argc != 2) {
-    directory = std::filesystem::canonical(std::filesystem::absolute("."));
-  } else {
-    directory = std::filesystem::absolute(argv[1]);
-  }
-
-  std::cout << "Profiling Directory " << directory << std::endl << std::endl;
-
-  if (!directory_validation::DirectoryExists(directory)) {
-    std::cerr << "FATAL: Directory " << directory << " does not exist!"
-              << std::endl;
-    return -1;
-  }
-
-  parser_info::Parser parser{};
-
-  int parse_result = parser.ParseFiles(directory);
-  if (parse_result) {
-    return parse_result;
-  }
-
-  //   int markdown_result = parser.DocumentFiles(directory);
-  //   if (markdown_result) {
-  //     return markdown_result;
-  //   }
-
-  return 0;
-}
+void GetFilesToProfile(const std::filesystem::path& directory,
+                       std::vector<std::filesystem::path>& found_files);
+}  // namespace directory_validation
+#endif  // INCLUDE_DIRECTORY_VALIDATOR_HPP_
