da4901026f9a77ed82360cfb8b2a31982af3221f
spineda2019 <spineda.wpi.alum@gmail.com>
2024-04-16 09:33:56 -0400

    change keyword array to have read-only str-views

--- src/include/Parser.hpp
+++ src/include/Parser.hpp
@@ -61,7 +61,7 @@ class Parser {
 
  private:
   std::unordered_map<std::string_view, std::size_t> file_type_frequencies_;
-  std::array<std::pair<const char*, std::size_t>, 4> keyword_pairs_;
+  std::array<std::pair<std::string_view, std::size_t>, 4> keyword_pairs_;
 
   std::mutex print_lock_;
   std::mutex markdown_lock_;
